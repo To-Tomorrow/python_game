68f399997f3628771c5e80e4bc730b65a4093fb6
jiajiakang <171197003@qq.com>
2019-05-09 10:19:45 +0800

    初始化

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,31 +1,24 @@
-import sys
-import pygame
 
+import pygame
 from settings import Settings
+from ship import Ship
+import game_functions as gf
 
 def run_game():
     #初始化游戏并创建一个屏幕对象
-    pygame.init();
-    ai_settimgs = Settings()
-    scree = pygame.display.set_mode((ai_settimgs.screen_width,ai_settimgs.screen_height);
-    pygame.display.set_caption("Alien Invasion");
+    pygame.init()
+    ai_settings = Settings();
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
 
-    #设置背景颜色
-    bg_color = (230,230,230);
+    #船舰一艘飞船
+    ship = Ship(ai_settings,screen)
 
-    #开始游戏主体循环
+    #开始游戏主循环
     while True:
-        #监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit();
-
-        #每次循环时都重绘屏幕
-        scree.fill(ai_settimgs.ba_color)
-
-        #让最近绘制的屏幕可见
-        pygame.display.flip();
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings,screen,ship)
 
 
-run_game();
-
+run_game()
